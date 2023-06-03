e269f2b05820fbb6161c7c03eefc29f4bb014d47
Antony Leme <antony040402@gmail.com>
2023-06-02 22:47:23 -0300

    fix: url

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -10,10 +10,10 @@ const Topbar: React.FC = function () {
       <Container maxW="container.lg">
         <HStack justifyContent="space-between">
           <HStack spacing="32px">
+            <Link as={NextLink} href="/">Vendas</Link>
             <Link as={NextLink} href="/products">Produtos</Link>
             <Link as={NextLink} href="/users">Usuários</Link>
             <Link as={NextLink} href="/sales-report">Relatório de Vendas</Link>
-            <Link as={NextLink} href="/sales">Vendas</Link>
             <Link as={NextLink} href="/stock">Estoque</Link>
           </HStack>
           <Link as={NextLink} href="/login">Sair</Link>
